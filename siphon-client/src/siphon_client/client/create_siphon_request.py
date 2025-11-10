4256a41b0620def4815f2c97e932611acccd60a6
Johnny Cakes <biden@phat.co.uk>
2025-11-09 19:55:50 -0600

    create siphon request function now works for urls and files

--- siphon-client/src/siphon_client/client/create_siphon_request.py
+++ siphon-client/src/siphon_client/client/create_siphon_request.py
@@ -1,12 +1,27 @@
-from pathlib import Path
 from siphon_api.requests import SiphonFile, SiphonRequest
 from siphon_api.enums import SourceOrigin
 from siphon_api.file_types import get_mime_type
+from urllib.parse import urlparse
+from pathlib import Path
 import hashlib
 import base64
 
 
-def create_siphon_request(file_path: Path) -> SiphonRequest:
+def determine_origin(source: str) -> SourceOrigin:
+    if Path(source).is_absolute() and Path(source).exists():
+        return SourceOrigin.FILE_PATH
+    else:
+        parsed = urlparse(source)
+        if parsed.scheme in {"http", "https"} or parsed.netloc:
+            return SourceOrigin.URL
+    raise ValueError("Source must be either a valid absolute file path or a valid URL.")
+
+
+def create_siphon_request_from_file(file_path: Path) -> SiphonRequest:
+    """
+    Create a SiphonRequest object from a file path.
+    Needs to be (1) an absolute path and (2) a valid file.
+    """
     data = file_path.read_bytes()
     checksum = hashlib.sha256(data).hexdigest()
     mime_type = get_mime_type(file_path.name)
@@ -24,3 +39,26 @@ def create_siphon_request(file_path: Path) -> SiphonRequest:
         origin=SourceOrigin.FILE_PATH,
         file=siphon_file,
     )
+
+
+def create_siphon_request_from_url(url: str) -> SiphonRequest:
+    """
+    Create a SiphonRequest object from a URL.
+    """
+    return SiphonRequest(
+        source=url,
+        origin=SourceOrigin.URL,
+        file=None,
+    )
+
+
+def create_siphon_request(source: str) -> SiphonRequest:
+    """
+    Create a SiphonRequest object from either a file path or a URL.
+    """
+    origin = determine_origin(source)
+    match origin:
+        case SourceOrigin.FILE_PATH:
+            return create_siphon_request_from_file(Path(source))
+        case SourceOrigin.URL:
+            return create_siphon_request_from_url(source)
